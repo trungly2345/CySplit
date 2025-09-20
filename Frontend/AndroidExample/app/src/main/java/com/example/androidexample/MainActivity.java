daf73764ba45831af6513ae8d0cf8e1372774797
OrangeJusu <91743185+OrangeJusu@users.noreply.github.com>
2025-09-20 15:19:03 -0500

    Git Test w/ View Profile Button

--- Frontend/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Frontend/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView usernameText;  // define username textview variable
     private Button loginButton;     // define login button variable
     private Button signupButton;    // define signup button variable
+    private Button profileButton;      // profile button variable
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         usernameText = findViewById(R.id.main_username_txt);// link to username textview in the Main activity XML
         loginButton = findViewById(R.id.main_login_btn);    // link to login button in the Main activity XML
         signupButton = findViewById(R.id.main_signup_btn);  // link to signup button in the Main activity XML
+        profileButton = findViewById(R.id.profile_btn);
 
         /* extract data passed into this activity from another activity */
         Bundle extras = getIntent().getExtras();
