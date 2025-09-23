1ab7ec4c2b7013adf7fa26f071b0b68981a7cdd4
OrangeJusu <91743185+OrangeJusu@users.noreply.github.com>
2025-09-23 14:37:21 -0500

    Added Profile Page w/ Menu nav bar

--- Frontend/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Frontend/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -72,5 +72,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        profileButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
