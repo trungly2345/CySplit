004ae3e146ef8ce605b7de76e746551b80e13974
OrangeJusu <91743185+OrangeJusu@users.noreply.github.com>
2025-09-30 14:40:49 -0500

    Fixed old login code to work with Fragment system

--- Frontend/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Frontend/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -15,27 +15,17 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView messageText;   // define message textview variable
-    private TextView usernameText;  // define username textview variable
-    private Button loginButton;     // define login button variable
-    private Button signupButton;    // define signup button variable
-    private Button profileButton;      // profile button variable
     private BottomNavigationView bottomNav;
     private HomeFragment homeFragment = new HomeFragment();
     private GroupFragment groupFragment = new GroupFragment();
     private ProfileFragment profileFragment = new ProfileFragment();
+    private LoginFragment loginFragment = new LoginFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);             // link to Main activity XML
 
-        /* initialize UI elements */
-        messageText = findViewById(R.id.main_msg_txt);      // link to message textview in the Main activity XML
-        usernameText = findViewById(R.id.main_username_txt);// link to username textview in the Main activity XML
-        loginButton = findViewById(R.id.main_login_btn);    // link to login button in the Main activity XML
-        signupButton = findViewById(R.id.main_signup_btn);  // link to signup button in the Main activity XML
-        profileButton = findViewById(R.id.profile_btn);
         bottomNav = findViewById(R.id.bottom_navigation);
 
         loadFragment(homeFragment);
@@ -52,61 +42,14 @@ public class MainActivity extends AppCompatActivity {
             else if (itemId == R.id.nav_profile){
                 loadFragment(profileFragment);
             }
+            else if (itemId == R.id.nav_login){
+                loadFragment(loginFragment);
+            }
 
             return true;
         });
 
-        /* extract data passed into this activity from another activity */
         Bundle extras = getIntent().getExtras();
-        if(extras == null) {
-            messageText.setText("Home Page");
-            usernameText.setVisibility(View.INVISIBLE);             // set username text invisible initially
-        } else {
-            messageText.setText("Welcome, " + extras.getString("USERNAME") + "!");
-            usernameText.setText(extras.getString("USERNAME")); // this will come from LoginActivity
-            loginButton.setVisibility(View.INVISIBLE);              // set login button invisible
-            signupButton.setVisibility(View.INVISIBLE);             // set signup button invisible
-        }
-        Button logoutButton = findViewById(R.id.main_logout_btn);
-        logoutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(intent);
-                finish();
-            }
-        });
-        /* click listener on login button pressed */
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                /* when login button is pressed, use intent to switch to Login Activity */
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        /* click listener on signup button pressed */
-        signupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                /* when signup button is pressed, use intent to switch to Signup Activity */
-                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        profileButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-
-                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 
     private boolean loadFragment(Fragment fragment){
