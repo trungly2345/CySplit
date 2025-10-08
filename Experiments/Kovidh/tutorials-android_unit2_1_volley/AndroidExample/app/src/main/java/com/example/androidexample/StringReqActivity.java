886df643d01b679f52105ca7d5dc3c7e735cd836
kovidh <kovidh@iastate.edu>
2025-10-07 22:24:50 -0500

    changes to exp

--- Experiments/Kovidh/tutorials-android_unit2_1_volley/AndroidExample/app/src/main/java/com/example/androidexample/StringReqActivity.java
+++ Experiments/Kovidh/tutorials-android_unit2_1_volley/AndroidExample/app/src/main/java/com/example/androidexample/StringReqActivity.java
@@ -24,7 +24,7 @@ public class StringReqActivity extends AppCompatActivity {
     private TextView msgResponse;
 
     // API URL for fetching string response
-    private static final String URL_STRING_REQ = "https://jsonplaceholder.typicode.com/users/1";
+    private static final String URL_STRING_REQ = "https://3a6da756-81eb-4ca2-b883-ffe1e492128b.mock.pstmn.io/groups";
     // Alternative URLs for testing purposes
     // public static final String URL_STRING_REQ = "https://2aa87adf-ff7c-45c8-89bc-f3fbfaa16d15.mock.pstmn.io/users/1";
     // public static final String URL_STRING_REQ = "http://10.0.2.2:8080/users/1";
