f38cf28f0d7d4d8a9516a006d8541d3e61b69a88
OrangeJusu <91743185+OrangeJusu@users.noreply.github.com>
2025-11-04 14:44:38 -0600

    Added A Base Setting Fragment (NOT FUNCTIONAL YET)

--- Frontend/AndroidExample/app/src/main/java/com/example/androidexample/ProfileFragment.java
+++ Frontend/AndroidExample/app/src/main/java/com/example/androidexample/ProfileFragment.java
@@ -2,13 +2,27 @@ package com.example.androidexample;
 
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class ProfileFragment extends Fragment {
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        Button settingsButton = view.findViewById(R.id.settings_button);
+
+        settingsButton.setOnClickListener(v -> {
+            FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+            transaction.replace(R.id.fragment_container, new SettingsFragment());
+            transaction.addToBackStack(null); // allows going back
+            transaction.commit();
+        });
+
+        return view;
     }
 }
