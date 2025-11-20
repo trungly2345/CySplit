9cb0bf600b704549b4dd9e74609a94b97ec1e206
Trung Ly <trungly@iastate.edu>
2025-11-19 23:29:00 -0600

    testing for main

--- Backend/springboot_manytoone/src/main/java/manytoone/Bills/BillRepository.java
+++ Backend/springboot_manytoone/src/main/java/manytoone/Bills/BillRepository.java
@@ -4,6 +4,8 @@ import manytoone.Groups.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+
+///  test for main 
 public interface BillRepository extends JpaRepository<Bill, Integer> {
 
 }
